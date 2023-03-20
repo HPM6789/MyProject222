521ff8c39559e7d45620c9b305d6124608bcb3b5
hieu <minhhieupham8888@gmail.com>
2023-03-20 16:36:59 +0700

    update project libary and templates

--- PRN231_Project_SP23/ProjectApi/Program.cs
+++ PRN231_Project_SP23/ProjectApi/Program.cs
@@ -1,10 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapControllers();
 
 app.Run();
