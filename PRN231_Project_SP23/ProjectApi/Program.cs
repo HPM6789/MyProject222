a5258d8da26de1c5a1fb0c7f04c36f2004068127
hieu <minhhieupham8888@gmail.com>
2023-03-20 16:11:52 +0700

    add project reference

--- PRN231_Project_SP23/ProjectApi/Program.cs
+++ PRN231_Project_SP23/ProjectApi/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapControllers();
 
 app.Run();
