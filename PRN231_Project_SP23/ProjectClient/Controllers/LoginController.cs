b88c3d234bf46ba06edf950ae6f0ec74a4948980
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-03-23 20:33:33 +0700

    LoginForm

---
+++ PRN231_Project_SP23/ProjectClient/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectClient.Controllers
+{
+    [Route("login")]
+    public class LoginController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("process")]
+        public IActionResult Process(string username, string myPassword)
+        {
+            //TODO: connect with database
+            if (username != null && myPassword != null && username.Equals("admin") && myPassword.Equals("123"))
+            {
+                HttpContext.Session.SetString("username", username);
+                return View("Welcome");
+            }
+            else
+            {
+                ViewBag.error = "Invalid";
+                return View("Index");
+            }
+        }
+    }
+}
