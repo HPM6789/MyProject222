fe1733803f4fa7b0b1d5f9dbc50475efbf577463
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-03-24 14:42:08 +0700

    Add Teacher Areas

--- PRN231_Project_SP23/Repository/Interfaces/IUserRepository.cs
+++ PRN231_Project_SP23/Repository/Interfaces/IUserRepository.cs
@@ -9,6 +9,11 @@ namespace Repository.Interfaces
 {
     public interface IUserRepository
     {
+        List<User> GetAllUsers();
+        User GetUserById(int uId);
+        void DeleteUser(User u);
+        void InsertUser(User u);
+        void UpdateProduct(User u);
         public User checkLogin(string email, string password);
         public string GetRoleByEmail(string email);
         public User GetUserByEmail(string email);
