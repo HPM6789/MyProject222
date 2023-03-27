18dd69f57c916cbb4d6a86851d43715d846ac7ae
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-03-27 11:18:00 +0700

    Update delete user

--- PRN231_Project_SP23/DataAccess/UserDao.cs
+++ PRN231_Project_SP23/DataAccess/UserDao.cs
@@ -82,8 +82,47 @@ namespace DataAccess
             {
                 using (var context = new PRN231_ProjectContext())
                 {
-                    //Todo: delete materials --> submit Assignment --> Assignments
+                    //Todo: delete materials --> submitAssignment --> Assignments
                     var p1 = context.Users.SingleOrDefault(c => c.UserId == u.UserId);
+                    var materials = context.Materials.Where(c => c.Uploader.UserId == p1.UserId).ToList();
+                    var assignments = context.Assignments.Where(c => c.Uploader.UserId == p1.UserId).ToList();
+                    //1. delete materials
+                    context.Materials.RemoveRange(materials);
+                    //2. delete submitAssignments of Ass
+                    foreach (var ass in assignments)
+                    {
+                        //delete assignments file
+                        if (File.Exists(ass.Path))
+                        {
+                            File.Delete(ass.Path);
+                        }
+                        var subAss = context.SubmitAssignments.Where(a => a.AssignmentId == ass.AssignmentId).ToList();
+                        foreach (var sub in subAss)
+                        {
+                            //delete SubAssignments file
+                            if (File.Exists(ass.Path))
+                            {
+                                File.Delete(ass.Path);
+                            }
+                        }
+                        context.SubmitAssignments.RemoveRange(subAss);
+                    }
+
+                    //3. delete ass
+                    context.Assignments.RemoveRange(assignments);
+                    //4. delete user course
+                    var c = context.Users.Include(c => c.Courses)
+                        .SingleOrDefault(c => c.UserId == u.UserId);
+
+                    var userCourse = c.Courses.ToList();
+                    foreach (var uc in userCourse)
+                    {
+                        c.Courses.Remove(uc);
+                    }
+                    //5. delete submitass of user
+                    var subAssignments = context.SubmitAssignments.Where(c => c.Uploader.UserId == p1.UserId).ToList();
+                    context.SubmitAssignments.RemoveRange(subAssignments);
+                    //delete user
                     context.Users.Remove(p1);
                     context.SaveChanges();
                 }
